264544a5cb07a7b2c3c91aa1d6e570b5c430d2fe
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-12-18 22:45:51 +0000

    adding tests

--- website/polls/models.py
+++ website/polls/models.py
@@ -7,13 +7,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
-def __str__(self):
+    def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 
-def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,5 +23,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 
-def __str__(self):
-        return self.choice_text
+    def __str__(self):
+            return self.choice_text
